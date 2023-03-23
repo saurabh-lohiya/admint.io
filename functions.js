a3052143a2c0622fc036309984cac4ca676b467f
Saurabh <saurabh.lohiya@rowisto.com>
2023-03-23 19:01:48 +0530

    Changed the logic for learn page

--- functions.js
+++ functions.js
@@ -1,7 +1,14 @@
-function loadWindow(){
+function loadWindow() {
+  console.log("Hello World")
 }
 
 
 window.onload = (event) => {
-//	document.getElementById("blurMe").style.filter = "blur(6px)";
+  //	document.getElementById("blurMe").style.filter = "blur(6px)";
 };
+
+
+function formValidation(event) {
+
+  console.log("Validation", event.target)
+}
