2faff45f05ab241656bb4d2c9fbdb3bd83952180
gsalunke <ganeshharshil@gmail.com>
2023-03-24 13:12:20 +0530

    fixing functions

--- functions.js
+++ functions.js
@@ -3,9 +3,6 @@ function loadWindow(){
 
 
 window.onload = (event) => {
-  document.getElementById("blurMe").style.filter = "blur(6px)";
-};
-
-window.onload = (event) => {
+// document.getElementById("blurMe").style.filter = "blur(6px)";
   document.getElementById("opacityme").style.filter = "blur(6px)";
 };
